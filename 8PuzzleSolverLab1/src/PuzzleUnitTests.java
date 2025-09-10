7523b1e083021ffc3c05842844e197844cb9789a
T1C0x <105113854+T1C0x@users.noreply.github.com>
2025-09-10 23:18:30 +0100

    BestFirst solve added, main fixed(?)

--- 8PuzzleSolverLab1/src/PuzzleUnitTests.java
+++ 8PuzzleSolverLab1/src/PuzzleUnitTests.java
@@ -1,23 +1,21 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PuzzleUnitTests {
-
     @Test
-    public void testConstructor(){
+    public void testConstructor() {
         Board b = new Board("023145678");
         StringWriter writer = new StringWriter();
-        PrintWriter pw = new PrintWriter(writer);
+        PrintWriter pw = new PrintWriter ( writer ) ;
         pw.println(" 23");
         pw.println("145");
         pw.println("678");
         assertEquals(b.toString(), writer.toString());
+        pw.close();
     }
-
     @Test
     public void testConstructor2() {
         Board b = new Board("123485670");
